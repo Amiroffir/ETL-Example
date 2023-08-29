a6c47bf98db81cf5d400d2f95a5a1a64fd058685
Amiroffir <amirosoffir@gmail.com>
2023-08-29 20:00:08 +0300

    commit

--- src/main/java/org/example/Extractor.java
+++ src/main/java/org/example/Extractor.java
@@ -62,4 +62,61 @@ public class Extractor {
 
         return fileList;
     }
+
+
+   /* public static Map<String, List<File>> extractFiles(String jobDbPath, String specificJobPath) {
+        Map<String, List<File>> folderFilesMap = new HashMap<>();
+
+        if (specificJobPath != null) {
+            File specificJobFolder = new File(jobDbPath, specificJobPath);
+            extractFromSubfolders(specificJobFolder, folderFilesMap);
+        } else {
+            File jobDbFolder = new File(jobDbPath);
+            extractFromSubfolders(jobDbFolder, folderFilesMap);
+        }
+
+        return folderFilesMap;
+    }
+
+    private static void extractFromSubfolders(File folder, Map<String, List<File>> folderFilesMap) {
+        File[] subfolders = folder.listFiles(File::isDirectory);
+
+        for (File subfolder : subfolders) {
+            if (containsFusion600(subfolder)) {
+                List<File> fileList = listFilesInFolder(subfolder);
+                folderFilesMap.put(subfolder.getAbsolutePath(), fileList);
+            }
+            extractFromSubfolders(subfolder, folderFilesMap); // Recurse into subfolders
+        }
+    }
+
+    private static boolean containsFusion600(File folder) {
+        File[] files = folder.listFiles();
+
+        for (File file : files) {
+            if (file.isDirectory() && file.getName().equals("fusion600")) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static List<File> listFilesInFolder(File folder) {
+        List<File> fileList = new ArrayList<>();
+        extractFilesFromFolderAndSubfolders(folder, fileList);
+        return fileList;
+    }
+
+    private static void extractFilesFromFolderAndSubfolders(File folder, List<File> fileList) {
+        File[] files = folder.listFiles();
+
+        for (File file : files) {
+            if (file.isFile()) {
+                fileList.add(file);
+            } else if (file.isDirectory()) {
+                extractFilesFromFolderAndSubfolders(file, fileList);
+            }
+        }
+    }*/
 }
